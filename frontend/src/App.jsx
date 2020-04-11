6e3a464f8f03d72afe1ad97e0c7c561dbd47eb9e
Yurematias <yurematias@ufrn.edu.br>
2020-04-11 03:16:03 -0300

    first commit in frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Routes from './components/Routes.jsx';
+import Footer from './components/Footer.jsx';
+
+function App() {
+    return (
+        <>
+            <Routes />
+            <Footer />
+        </>
+    )
+}
+
+export default App;
