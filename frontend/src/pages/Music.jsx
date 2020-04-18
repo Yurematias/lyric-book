0c2f63a1b43b251cf89b85f42e6869b11c10b7c0
Yurematias <yurematias@ufrn.edu.br>
2020-04-17 21:51:12 -0300

    music page updated

--- frontend/src/pages/Music.jsx
+++ frontend/src/pages/Music.jsx
@@ -5,16 +5,49 @@ import './css/music.css';
 function Music() {
     return (
         <section id="music-container">
-            <Title style={{fontSize: '30px'}} />
+            <Title style={{fontSize: '55px'}} />
             <header>
                 <h2>Título da música</h2>
                 <div>
-                    <button>Voltar</button>
-                    <button>Salvar</button>
+                    <button className="back">Voltar</button>
+                    <button className="save">Salvar</button>
                 </div>
             </header>
             <article className="lyric-area">
-            
+                It's not in the way that you hold me
+                it's not in the way you say you care
+                It's not in the way you've been treating my friends
+                It's not in the way that you stayed till the end
+                It's not in the way you look or the things that you say that you'll do
+
+                Hold the line, love isn't always on time, oh oh oh
+                Hold the line, love isn't always on time, oh oh oh
+
+                It's not in the words that you told me, girl
+                It's not in the way you say you're mine
+                It's not in the way that you came back to me
+                It's not in the way that your love set me free
+                It's not in the way you look or the things that you say that you'll do
+
+                Hold the line, love isn't always on time, oh oh oh
+                Hold the line, love isn't always on time, oh oh oh
+
+                It's not in the words that you told me
+                It's not in the way you say you're mine
+                It's not in the way that you came back to me
+                It's not in the way that your love set me free
+                It's not in the way you look or the things that you say that you'll do
+
+                Hold the line, love isn't always on time oh oh oh
+                Hold the line, love isn't always on the time
+                Love isn't always on time
+
+                Hold the line, love isn't always on time
+                Love isn't always, love isn't always on time
+                Hold the line, love isn't always on time
+                Love isn't always on time
+                Love isn't always on time
+                Love isn't always on time oh oh oh
             </article>
         </section>
     );
